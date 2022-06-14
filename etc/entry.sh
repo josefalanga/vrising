bd3b13c20f46ac2fc0e8920113849c0568182c64
Jose Falanga <jose.falanga@etermax.com>
2022-06-14 13:28:59 -0300

    initial commit

---
+++ etc/entry.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir -p "${STEAMAPPDIR}" || true  
+
+bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
+				+login anonymous \
+				+app_update "${STEAMAPPID}" \
+				+quit
+
+##TODO: Get the actual vrising config dynamically
+# Is the config missing?
+# if [ ! -f "${STEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg" ]; then
+# 	# Download & extract the config
+# 	wget -qO- "${DLURL}/master/etc/cfg.tar.gz" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"
+
+# 	# Change hostname on first launch (you can comment this out if it has done its purpose)
+# 	sed -i -e 's/{{SERVER_HOSTNAME}}/'"${SRCDS_HOSTNAME}"'/g' "${STEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg"
+# fi
+
+cd "${STEAMAPPDIR}"
+
+export WINEARCH=win64
+
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine VRisingServer.exe -persistentDataPath ./save-data -logFile server.log 
