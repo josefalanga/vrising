5e7bc7042d600929b1616cffac2363ac8ba6e0e2
Jose Falanga <jose.falanga@etermax.com>
2022-06-17 02:00:29 -0300

    playme

--- play.sh
+++ play.sh
@@ -1,1 +1,3 @@
+docker system prune -a && docker build . -t vrising
 docker run -d -it --name vrising-container -v vrising-storage:/home/steam/vrising-server-dedicated/save-data vrising
+sudo docker exec -u 0 vrising-container chown steam:steam /home/steam/vrising-server-dedicated/save-data
