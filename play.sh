e8adf92ce430bc770e6b9e3745b8460c5a587e73
Jose Falanga <jose.falanga@etermax.com>
2022-06-22 20:14:25 -0300

    add post login for shit and giggles

--- play.sh
+++ play.sh
@@ -4,3 +4,4 @@ docker build . -t vrising
 DATA="/home/steam/vrising-server-dedicated/save-data"
 docker run -d -it --name vrising-container -v "vrising-storage:$DATA" vrising
 sudo docker exec -u 0 vrising-container chown steam:steam "$DATA"
+sudo docker exec -it vrising-container /bin/sh
