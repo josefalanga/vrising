08ba53c5b23007deb26a8a08d80ddc1e5efa5f85
Jose Falanga <jose.falanga@etermax.com>
2022-06-23 13:22:34 -0300

    wait for screen to end

--- etc/entry.sh
+++ etc/entry.sh
@@ -22,4 +22,8 @@ cd "${STEAMAPPDIR}"
 
 export WINEARCH=win64
 
-screen -dmS vrising xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine VRisingServer.exe -persistentDataPath ./save-data -logFile server.log 
+screen -dmS vrising xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine VRisingServer.exe -persistentDataPath ./save-data -logFile server.log
+while screen -list | grep -q vrising
+do
+    sleep 1
+done
