6ec3039badb4cd938426ac1368a46f86599f62e9
Jose Falanga <jose.falanga@etermax.com>
2022-06-23 19:36:57 -0300

    expose ports

--- play.sh
+++ play.sh
@@ -2,6 +2,6 @@ docker stop vrising-container
 docker system prune -a
 docker build . -t vrising
 DATA="/home/steam/vrising-server-dedicated/save-data"
-docker run -d -it --name vrising-container -v "vrising-storage:$DATA" vrising
+docker run -d -it --name vrising-container -p 27015-27036:27015-27036 -v "vrising-storage:$DATA" vrising
 sudo docker exec -u 0 vrising-container chown steam:steam "$DATA"
 sudo docker exec -it vrising-container /bin/sh
