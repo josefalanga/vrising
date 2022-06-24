8a4021196ae0dc5f9988d29b25080ac3b94c6cdb
Jose Falanga <jose.falanga@etermax.com>
2022-06-24 09:26:16 -0300

    only purge container and image at start of build process

--- play.sh
+++ play.sh
@@ -1,5 +1,5 @@
-docker stop vrising-container
-docker system prune -a
+docker container rm -f vrising-container
+docker image rm -f vrising
 docker build . -t vrising
 DATA="/home/steam/vrising-server-dedicated/save-data"
 docker run -d -it --name vrising-container -p 27015-27036:27015-27036 -p 9876:9876 -p 9877:9877 -p 25575:25575 -v "vrising-storage:$DATA" vrising
